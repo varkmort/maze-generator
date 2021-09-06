9d74582769f03f7ecf13b657ea89c09c72fc7bbe
varkmort <vitsibart@gmail.com>
2021-09-06 10:14:25 +0500

    fill algorithm
    add base structure to algorithm

--- maze generator/MazeBuilder.cpp
+++ maze generator/MazeBuilder.cpp
@@ -19,17 +19,27 @@ void MazeBuilder::setStartPoint(Coord coord) {
 }
 
 void MazeBuilder::build() {
+    std::random_device generator;
+    std::uniform_int_distribution<int> distribution(1,6);
+    int dice_roll = distribution(generator);
     std::stack<Coord> path;
     std::set<Coord> visited;
-    //add current position in stack
-    path.push(startPoint);
-    //get possible directions
-    //reserve 1 dirrection
-    //1-n rest fill with walls
-    //make step
-    //back to 1 until we have atleast 1 direction per step
-    //else backtrack until we got step with 1 or more dirrection
-    //end when we back to start and have 0 direction
+    Coord current{startPoint};
+    while (false/*end when we back to start and have 0 direction*/)
+    {
+        //add current position in stack
+        path.push(current);
+        visited.insert(current);
+        //get possible directions
+
+        //reserve 1 dirrection
+        //1-n rest fill with walls
+        //make step
+        //back to 1 until we have atleast 1 direction per step
+        //else backtrack until we got step with 1 or more dirrection
+    }
+    
+    
 }
 
 void MazeBuilder::free() {
