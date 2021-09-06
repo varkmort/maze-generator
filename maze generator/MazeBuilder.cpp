a672ebeb376a0451f127eda53e9e83a5a7ab2a05
varkmort <vitsibart@gmail.com>
2021-09-06 13:46:46 +0500

    add realization of builder

--- maze generator/MazeBuilder.cpp
+++ maze generator/MazeBuilder.cpp
@@ -1,12 +1,7 @@
 #include "MazeBuilder.hpp"
 
 
-#include <random>
-#include <chrono>
-#include <algorithm>
-#include <vector>
-#include <set>
-#include <stack>
+
 
 MazeBuilder::MazeBuilder(Map *map, Coord startPoint):data(map), startPoint(startPoint){}
 
@@ -22,27 +17,46 @@ void MazeBuilder::build() {
     std::random_device generator;
     std::uniform_int_distribution<int> distribution(1,6);
     int dice_roll = distribution(generator);
-    std::stack<Coord> path;
-    std::set<Coord> visited;
-    Coord current{startPoint};
-    while (false/*end when we back to start and have 0 direction*/)
+    current=startPoint;
+    while (!isMazeComplite())
     {
-        //add current position in stack
+        //get possible directions
+        auto directions = getDirections(current);
+        while(!directions.size() || isMazeComplite()){
+            current = path.top();
+            path.pop();
+            directions = getDirections(current);
+        }
+        if(directions.size()>1){
+            std::shuffle(directions.begin(),directions.end(),generator);
+            std::uniform_int_distribution<int> distribution(0,directions.size()-1);
+            auto it = directions.rbegin();
+            switch(distribution(generator)){
+                case 3:
+                    data->setWall(*it);
+                    ++it;
+                case 2:
+                    data->setWall(*it);
+                    ++it;
+                case 1:
+                    data->setWall(*it);
+                    ++it;
+                default:
+                break;
+            }
+        }
+        //make step
         path.push(current);
         visited.insert(current);
-        //get possible directions
-
-        //reserve 1 dirrection
-        //1-n rest fill with walls
-        //make step
-        //back to 1 until we have atleast 1 direction per step
-        //else backtrack until we got step with 1 or more dirrection
+        current = *directions.begin();
     }
     
     
 }
 
 void MazeBuilder::free() {
+    while(!path.empty())path.pop();
+    visited=std::set<Coord>();
     data = nullptr;
 }
 
@@ -53,3 +67,26 @@ Map *MazeBuilder::getMap() const {
 Coord MazeBuilder::getStartPoint() const {
     return startPoint;
 }
+
+std::vector<Coord> MazeBuilder::getDirections(Coord coord){
+    std::vector<Coord> result;
+    for(auto &dirrection:{up(coord), down(coord), left(coord), right(coord)}){
+        if(isPossibleway(dirrection)){
+            result.push_back(dirrection);
+        }   
+    }
+   return result;   
+}
+
+bool MazeBuilder::isMazeComplite(){
+    return bool(current==startPoint && !getDirections(current).size());
+}
+
+bool MazeBuilder::isPossibleway(Coord coord){
+    return !data->isWall(coord) && !visited.contains(coord);
+}
+
+Coord MazeBuilder::up(Coord coord){return coord.x+=1,coord;}
+Coord MazeBuilder::down(Coord coord){return coord.x-=1,coord;}
+Coord MazeBuilder::left(Coord coord){return coord.y-=1,coord;}
+Coord MazeBuilder::right(Coord coord){return coord.y+=1,coord;}
--- maze generator/MazeBuilder.hpp
+++ maze generator/MazeBuilder.hpp
@@ -1,7 +1,11 @@
 #pragma once
-
-#include"Map.hpp"
-
+#include "Map.hpp"
+#include <random>
+#include <chrono>
+#include <algorithm>
+#include <vector>
+#include <set>
+#include <stack>
 
 class MazeBuilder {
 public:
@@ -12,8 +16,18 @@ public:
 	void free();
 	Map *getMap()const;
 	Coord getStartPoint()const;
+	bool isMazeComplite();
 private:
+	std::vector<Coord> getDirections(Coord);
+	Coord up(Coord coord);
+	Coord down(Coord coord);
+	Coord left(Coord coord);
+	Coord right(Coord coord);
+	bool isPossibleway(Coord coord);
 	Map *data;
 	Coord startPoint;
+	Coord current;
+	std::stack<Coord> path;
+    std::set<Coord> visited;
 };
 
